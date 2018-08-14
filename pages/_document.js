f07cdb393abf0353290fa203cc3ee81ac5066cb7
Ronny Haase <ronnyhaase@fastmail.com>
2018-08-14 18:10:27 +0100

    Extend document structure
    - SSR of styled-components
    - Inject styled-components ThemeProvider
    - Global styles

---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import React from 'react'
+import { ServerStyleSheet, injectGlobal } from 'styled-components'
+
+import globalStyles from '../styles/global'
+
+injectGlobal`${globalStyles}`
+
+export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
+  }
+
+  render () {
+    return (
+      <html>
+        <Head>
+          <meta charSet="utf-8" />
+          <title>Ronny Haase</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no, viewport-fit=cover" />
+          <meta name="theme-color" content="#000000" />
+          <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400|Open+Sans" rel="stylesheet" />
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ styles/global.js
@@ -0,0 +1,40 @@
+import { font, color } from './variables'
+
+export default `
+  html {
+    box-sizing: border-box;
+    font-size: ${font.size.base}px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    font-family: ${font.family.sans};
+    font-size: ${font.size.md};
+    line-height: 1.75;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+    padding: 1em 0;
+    line-height: 1;
+  }
+
+  h1 {
+    font-size: ${font.size.xl}
+  }
+
+  h2 {
+    font-size: ${font.size.lg}
+  }
+
+  a {
+    color: ${color.link}
+  }
+`
