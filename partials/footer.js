15c65858f251b08abd130262726946038f4428e7
Ronny Haase <ronnyhaase@fastmail.com>
2018-08-21 11:15:11 +0100

    Fix diosallowed nesting inside <p>

--- partials/footer.js
+++ partials/footer.js
@@ -10,8 +10,8 @@ const Footer = ({ theme }) => (
         Copyright &copy; Ronny Haase, 2011 - 2018<br />
         <small>The <a href="https://github.com/ronnyhaase/ronnyhaase.com">code of this website</a> is open source under <a href="https://opensource.org/licenses/GPL-3.0">GPL v3</a></small>
       </p>
+      <p pb=".5rem"><a href="#">Back to Top</a></p>
       <p>
-        <Box pb=".5rem"><a href="#">Back to Top</a></Box>
         <a href="https://github.com/ronnyhaase">GitHub</a>&nbsp;&bull;&nbsp;
         <a href="https://linkedin.com/in/ronnyhaase">LinkedIn</a>&nbsp;&bull;&nbsp;
         <a href="https://twitter.com/ronnyhaase">Twitter</a>
