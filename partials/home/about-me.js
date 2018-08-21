a822744e52d8f1aca1ec9bda7881a9f7da9a0568
Ronny Haase <ronnyhaase@fastmail.com>
2018-08-21 09:56:05 +0100

    Add CV

--- partials/home/about-me.js
+++ partials/home/about-me.js
@@ -1,8 +1,9 @@
-import React from 'react'
-
-import { Container, Headline, Text } from '../../components'
-import { color, font } from '../../styles/variables'
 import { Box } from 'grid-styled'
+import React from 'react'
+import { FileText } from 'react-feather'
+
+import { Button, Container, Headline, Text } from '../../components'
+import { color, font } from '../../styles/variables'
 
 const AboutMe = () => (
   <div id="about-me">
@@ -14,6 +15,12 @@ const AboutMe = () => (
       </Container>
     </Box>
     <Container py="1rem">
+      <center>
+        <Button is="a" href="/static/cv-ronnyhaase.pdf" boxShadow="2px 2px 6px rgba(0,0,0,.5)" >
+          <FileText />
+          <span>Download my CV (PDF)</span>
+        </Button>
+      </center>
       <Text fontFamily={font.family.sans}>
         <p>Hi! I am Ronny.</p>
         <p>
@@ -26,6 +33,9 @@ const AboutMe = () => (
           products (MVE to massive scale), and agile teams of different size and experience - also
           taking leadership responsibilities - and was giving presentations, training and mentorship
           on front-end and architecture topics either in general or for concrete projects.
+        </p>
+        <p>
+          Other than that I love posting Emperor Palpatine GIFs and massively using <code>/shrug</code> on Slack. ¯\_(ツ)_/¯
         </p>
       </Text>
     </Container>
