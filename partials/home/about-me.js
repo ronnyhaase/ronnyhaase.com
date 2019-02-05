df999cad7c66290f952098d62a6e71ab12ea65e2
Ronny Haase <ronnyhaase@fastmail.com>
2019-02-05 13:43:11 +0000

    Update About me content

--- partials/home/about-me.js
+++ partials/home/about-me.js
@@ -23,18 +23,13 @@ const AboutMe = () => (
       <Text fontFamily={font.family.sans}>
         <p>Hi! I am Ronny.</p>
         <p>
-          I'm developing web based solutions for more than 10 years with focus but not limited to
-          front-end development. I offer excellent knowledge and multiple years of experience with
-          designing, developing, testing and optimizing scalable web solutions.
+          I'm developing reliable, maintainable and scalable web based solutions for more than 10
+          years with focus but by no means limited to front-end engineering, and built and lead great
+          teams to do the same.<br />
+          I offer excellent knowledge and multiple years of experience with designing, developing, testing and optimizing highly-scalable web solutions as well as enabling teams to be productive & motivated, and grow personally and professionally.
         </p>
         <p>
-          I've been working with companies at different stages (Startup to DAX-100), as well as
-          products (MVE to massive scale), and agile teams of different size and experience - also
-          taking leadership responsibilities - and was giving presentations, training and mentorship
-          on front-end and architecture topics either in general or for concrete projects.
-        </p>
-        <p>
-          Other than that I love posting Emperor Palpatine GIFs and massively using <code>/shrug</code> on Slack. ¯\_(ツ)_/¯
+          I've been working and consulting for companies at different stages (Startup to DAX-100), as well as products (MVE to massive scale), and agile teams of different size and experience. I am giving presentations, training and mentorship on front-end and software-architecture topics either in general or for concrete projects.
         </p>
       </Text>
     </Container>
