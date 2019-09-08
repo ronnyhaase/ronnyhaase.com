11e8e8d9741d89902173fb792232ec4f82f1999e
Ronny Haase <ronnyhaase@fastmail.com>
2019-09-08 23:15:16 +0100

    Improve static build script
    ... to consider all fiels

--- build/static.js
+++ build/static.js
@@ -1,8 +1,26 @@
 // TODO: Optimize optimizable assets (e.g. images)
 const fs = require('fs-extra')
+const glob = require('glob')
+const path = require('path')
 
-;(function copyStaticAssets() {
-  fs.ensureDirSync(process.cwd() + '/dist')
+const defaultOptions = {
+  pattern: '**/*',
+  inDir: 'static/',
+  outDir: 'dist/static/',
+}
 
-  fs.copySync(process.cwd() + '/static', process.cwd() + '/dist/static')
-})()
+const options = { ...defaultOptions }
+
+void async function () {
+  const { pattern, inDir, outDir } = options
+
+  const files = glob.sync(`${inDir}${pattern}`)
+
+  files.forEach(file => {
+    const fn = file
+      .replace(inDir, outDir)
+
+    fs.ensureDirSync(path.dirname(fn))
+    fs.copySync(file, fn)
+  })
+}()
