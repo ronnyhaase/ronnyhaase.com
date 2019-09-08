5a6d115658545cdadbb45a32cf8afb123f14d4a7
Ronny Haase <ronnyhaase@fastmail.com>
2019-09-08 23:10:40 +0100

    Improve HTML build script
    ... to consider all files not starting with _

--- build/html.js
+++ build/html.js
@@ -1,11 +1,29 @@
 const fs = require('fs-extra')
+const glob = require('glob')
+const path = require('path')
 const pug = require('pug')
 
-;(function renderHtml () {
-  fs.ensureDirSync(process.cwd() + '/dist')
+const defaultOptions = {
+  pattern: '**/!(_)*.pug',
+  inDir: 'templates/',
+  outDir: 'dist/',
+}
 
-  fs.writeFileSync(
-    process.cwd() + '/dist/index.html',
-    pug.renderFile('templates/index.pug', {}),
-  )
-})()
+const options = { ...defaultOptions }
+
+void async function () {
+  const { pattern, inDir, outDir } = options
+
+  const files = glob.sync(`${inDir}${pattern}`)
+
+  files.forEach(file => {
+    const htmlFn = file
+      .replace(inDir, outDir)
+      .replace(/\.pug/, '.html')
+
+    const html = pug.renderFile(file, {})
+
+    fs.ensureDirSync(path.dirname(htmlFn))
+    fs.writeFileSync(htmlFn, html)
+  })
+}()
