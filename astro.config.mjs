86507ff1d51d7855a64b76a03390edd4be1534ec
Ronny Haase <ronnyhaase@fastmail.com>
2023-02-05 00:16:27 +0100

    Optimize images via @astro/images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,21 @@
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
-
-import { defineConfig } from 'astro/config';
-import compress from "astro-compress";
+import image from '@astrojs/image'
+import prefetch from '@astrojs/prefetch'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
 
 export default defineConfig({
   site: 'https://ronnyhaase.com',
   integrations: [
     prefetch(),
     react(),
-    sitemap({
-      lastmod: new Date(),
-    }),
+    sitemap({ lastmod: new Date() }),
     tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
+      config: { applyBaseStyles: false },
     }),
     compress(),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
   ]
 });
