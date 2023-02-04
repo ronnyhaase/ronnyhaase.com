16ccf453fcd3f34d1a4a30c49a8e2c8fee616f4a
Ronny Haase <ronnyhaase@fastmail.com>
2023-02-04 17:39:39 +0100

    Add compression and configure sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,24 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
+import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
+
 export default defineConfig({
+  site: 'https://ronnyhaase.com',
   integrations: [
     prefetch(),
     react(),
-    sitemap(),
-    tailwind({ config: { applyBaseStyles: false } }),
-  ],
+    sitemap({
+      lastmod: new Date(),
+    }),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    compress(),
+  ]
 });
