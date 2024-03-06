4cdcdf042607de51adc46503210b89847aaa9acc
Ronny Haase <ronnyhaase@fastmail.com>
2024-03-06 09:13:04 +0100

    Upgrade to Astro v4 and pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
-import image from '@astrojs/image'
-import prefetch from '@astrojs/prefetch'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
-import compress from 'astro-compress'
+import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+import compress from "astro-compress"
 
 export default defineConfig({
-  site: 'https://ronnyhaase.com',
-  integrations: [
-    prefetch(),
-    react(),
-    sitemap({ lastmod: new Date() }),
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-    compress(),
-    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
-  ]
-});
+	site: "https://ronnyhaase.com",
+	prefetch: true,
+	integrations: [
+		react(),
+		sitemap({ lastmod: new Date() }),
+		tailwind({
+			config: { applyBaseStyles: false },
+		}),
+		compress(),
+	],
+})
