846739bd4e8ac717a2d48018e8d82bf4e3eab774
Ronny Haase <ronnyhaase@fastmail.com>
2024-05-20 21:46:45 +0200

    Add resumé
    - Update deps
    - Use component instead of global styles for links
    - Format code

--- src/scripts.js
+++ src/scripts.js
@@ -1,47 +1,54 @@
-function debounce (fn, wait) {
-  let t
-  return function () {
-    clearTimeout(t)
-    t = setTimeout(() => fn.apply(this, arguments), wait)
-  }
+function debounce(fn, wait) {
+	let t
+	return function () {
+		clearTimeout(t)
+		t = setTimeout(() => fn.apply(this, arguments), wait)
+	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Run animations
-  document.querySelectorAll('.animate-paused').forEach(el => {
-    el.style.willChange = 'transform, opacity'
-    el.classList.toggle('opacity-0')
-    el.classList.remove('animate-direction-reverse')
-    el.classList.remove('animate-paused')
-    el.classList.add('animate-running')
-  })
-  setTimeout(
-    () => document
-      .querySelectorAll('.animate-running')
-      .forEach(el => el.style.willChange = 'auto')
-    , 2000
-  )
+	// Run animations
+	document.querySelectorAll(".animate-paused").forEach((el) => {
+		el.style.willChange = "transform, opacity"
+		el.classList.toggle("opacity-0")
+		el.classList.remove("animate-direction-reverse")
+		el.classList.remove("animate-paused")
+		el.classList.add("animate-running")
+	})
+	setTimeout(
+		() =>
+			document
+				.querySelectorAll(".animate-running")
+				.forEach((el) => (el.style.willChange = "auto")),
+		2000,
+	)
 
-  // Smooth scroll
-  const samePageAnchorElements = document.querySelectorAll('a[href^="#"]')
-  samePageAnchorElements.forEach(el => el.addEventListener('click', ev => {
-    ev.preventDefault()
+	// Smooth scroll
+	const samePageAnchorElements = document.querySelectorAll('a[href^="#"]')
+	samePageAnchorElements.forEach((el) =>
+		el.addEventListener("click", (ev) => {
+			ev.preventDefault()
 
-    const scrollTarget = document.querySelector(
-      ev.currentTarget.getAttribute('href')
-    )
-    const top = scrollTarget.getBoundingClientRect().top + window.scrollY
-    window.scrollTo({ top, behavior: 'smooth' })
-  }))
+			const scrollTarget = document.querySelector(
+				ev.currentTarget.getAttribute("href"),
+			)
+			const top = scrollTarget.getBoundingClientRect().top + window.scrollY
+			window.scrollTo({ top, behavior: "smooth" })
+		}),
+	)
 
-  // Blur intro on scroll
-  const updateParallaxEffect = () => {
-    const currentScrollY = window.scrollY
-    const maxScrollY = document.body.clientHeight
-    const relativeScrollY = Math.max(0, (currentScrollY / maxScrollY))
-    document.getElementById('intro').style.filter = `blur(${relativeScrollY * 5}px)`
-  }
-  document.addEventListener('scroll', debounce(ev => {
-    updateParallaxEffect()
-  }, 1))
+	// Blur intro on scroll
+	const updateParallaxEffect = () => {
+		const currentScrollY = window.scrollY
+		const maxScrollY = document.body.clientHeight
+		const relativeScrollY = Math.max(0, currentScrollY / maxScrollY)
+		document.getElementById("intro").style.filter =
+			`blur(${relativeScrollY * 5}px)`
+	}
+	document.addEventListener(
+		"scroll",
+		debounce((ev) => {
+			updateParallaxEffect()
+		}, 1),
+	)
 })
